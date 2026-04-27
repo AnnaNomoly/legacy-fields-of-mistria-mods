25dabf8f07d70f0e7847e245c6b1dbb3acd8b822
AnnaNomoly <annanomoly@outlook.com>
2026-04-27 11:06:49 -0400

    Enemy Health Bars
    Added config file with options for smaller_health_bar and red_health_bar, both default to false.

--- EnemyHealthBars/source/ModuleMain.cpp
+++ EnemyHealthBars/source/ModuleMain.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <filesystem>
 #include <unordered_set>
@@ -12,8 +13,14 @@ static const char* const MOD_NAME = "EnemyHealthBars";
 static const char* const VERSION = "1.0.0";
 static const char* const GML_SCRIPT_DRAW_MONSTER = "gml_Script_draw@gml_Object_par_monster_Create_0";
 static const char* const GML_SCRIPT_SETUP_MAIN_SCREEN = "gml_Script_setup_main_screen@TitleMenu@TitleMenu";
+static const char* const SMALLER_HEALTH_BAR_JSON_KEY = "smaller_health_bar";
+static const char* const RED_HEALTH_BAR_JSON_KEY = "red_health_bar";
+static const bool DEFAULT_SMALLER_HEALTH_BAR = false;
+static const bool DEFAULT_RED_HEALTH_BAR = false;
 static const int HEALTH_BAR_HEIGHT = 9;
 static const int HEALTH_BAR_WIDTH = 40;
+static const int SMALL_HEALTH_BAR_HEIGHT = 9;
+static const int SMALL_HEALTH_BAR_WIDTH = 30;
 static const std::map<std::string, int> MONSTER_NAME_TO_SPRITE_SIZE_MAP = { // Uses the monster's "idle south" sprite, divided by 2 or 3
 	{ "bat", 40 * 0.65 }, // spr_monster_essence_bat_main_idle_south
 	{ "bat_blue", 40 * 0.65 }, // spr_monster_essence_bat_blue_main_idle_south
@@ -54,11 +61,139 @@ static const std::map<std::string, int> MONSTER_NAME_TO_SPRITE_SIZE_MAP = { // U
 	//{ "",  }, // 
 };
 
+struct Configuration {
+	bool smaller_health_bar = DEFAULT_SMALLER_HEALTH_BAR;
+	bool red_health_bar = DEFAULT_RED_HEALTH_BAR;
+};
+
 static YYTKInterface* g_ModuleInterface = nullptr;
 static CInstance* global_instance = nullptr;
+static Configuration configuration = Configuration();
 static bool load_on_start = true;
 static std::map<std::string, int> monster_name_to_id_map = {};
 static std::map<int, std::string> monster_id_to_name_map = {};
+
+void PrintError(std::exception_ptr eptr)
+{
+	try {
+		if (eptr) {
+			std::rethrow_exception(eptr);
+		}
+	}
+	catch (const std::exception& e) {
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Error: %s", MOD_NAME, VERSION, e.what());
+	}
+}
+
+json CreateConfigJson(bool use_defaults)
+{
+	json config_json = {
+		{ SMALLER_HEALTH_BAR_JSON_KEY, use_defaults ? DEFAULT_SMALLER_HEALTH_BAR : configuration.smaller_health_bar },
+		{ RED_HEALTH_BAR_JSON_KEY, use_defaults ? DEFAULT_RED_HEALTH_BAR : configuration.red_health_bar }
+	};
+	return config_json;
+}
+
+void CreateOrLoadConfigFile()
+{
+	std::exception_ptr eptr;
+	try
+	{
+		std::string current_dir = std::filesystem::current_path().string();
+		std::string mod_data_folder = current_dir + "\\mod_data";
+		if (!std::filesystem::exists(mod_data_folder))
+		{
+			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - The \"mod_data\" directory was not found. Creating directory: %s", MOD_NAME, VERSION, mod_data_folder.c_str());
+			std::filesystem::create_directory(mod_data_folder);
+		}
+
+		std::string enemy_health_bars_folder = mod_data_folder + "\\EnemyHealthBars";
+		if (!std::filesystem::exists(enemy_health_bars_folder))
+		{
+			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - The \"EnemyHealthBars\" directory was not found. Creating directory: %s", MOD_NAME, VERSION, enemy_health_bars_folder.c_str());
+			std::filesystem::create_directory(enemy_health_bars_folder);
+		}
+
+		bool update_config_file = false;
+		std::string config_file = enemy_health_bars_folder + "\\" + "EnemyHealthBars.json";
+		std::ifstream in_stream(config_file);
+		if (in_stream.good())
+		{
+			try
+			{
+				json json_object = json::parse(in_stream);
+
+				if (json_object.empty())
+				{
+					g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - No values found in mod configuration file: %s!", MOD_NAME, VERSION, config_file.c_str());
+					g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Add your desired values to the configuration file, otherwise defaults will be used.", MOD_NAME, VERSION);
+				}
+				else
+				{
+					if (json_object.contains(SMALLER_HEALTH_BAR_JSON_KEY))
+					{
+						configuration.smaller_health_bar = json_object[SMALLER_HEALTH_BAR_JSON_KEY];
+						g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Using CUSTOM \"%s\" value: %s!", MOD_NAME, VERSION, SMALLER_HEALTH_BAR_JSON_KEY, configuration.smaller_health_bar ? "true" : "false");
+					}
+					else
+					{
+						g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Missing \"%s\" value in mod configuration file: %s!", MOD_NAME, VERSION, SMALLER_HEALTH_BAR_JSON_KEY, config_file.c_str());
+						g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Using DEFAULT \"%s\" value: %s!", MOD_NAME, VERSION, SMALLER_HEALTH_BAR_JSON_KEY, DEFAULT_SMALLER_HEALTH_BAR ? "true" : "false");
+					}
+
+					if (json_object.contains(RED_HEALTH_BAR_JSON_KEY))
+					{
+						configuration.red_health_bar = json_object[RED_HEALTH_BAR_JSON_KEY];
+						g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Using CUSTOM \"%s\" value: %s!", MOD_NAME, VERSION, RED_HEALTH_BAR_JSON_KEY, configuration.red_health_bar ? "true" : "false");
+					}
+					else
+					{
+						g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Missing \"%s\" value in mod configuration file: %s!", MOD_NAME, VERSION, RED_HEALTH_BAR_JSON_KEY, config_file.c_str());
+						g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Using DEFAULT \"%s\" value: %s!", MOD_NAME, VERSION, RED_HEALTH_BAR_JSON_KEY, DEFAULT_RED_HEALTH_BAR ? "true" : "false");
+					}
+				}
+
+				update_config_file = true;
+			}
+			catch (...)
+			{
+				eptr = std::current_exception();
+				PrintError(eptr);
+
+				g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to parse JSON from configuration file: %s", MOD_NAME, VERSION, config_file.c_str());
+				g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Make sure the file is valid JSON!", MOD_NAME, VERSION);
+			}
+
+			in_stream.close();
+		}
+		else
+		{
+			in_stream.close();
+
+			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - The \"EnemyHealthBars.json\" file was not found. Creating file: %s", MOD_NAME, VERSION, config_file.c_str());
+
+			json default_config_json = CreateConfigJson(true);
+			std::ofstream out_stream(config_file);
+			out_stream << std::setw(4) << default_config_json << std::endl;
+			out_stream.close();
+		}
+
+		if (update_config_file)
+		{
+			json config_json = CreateConfigJson(false);
+			std::ofstream out_stream(config_file);
+			out_stream << std::setw(4) << config_json << std::endl;
+			out_stream.close();
+		}
+	}
+	catch (...)
+	{
+		eptr = std::current_exception();
+		PrintError(eptr);
+
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - An error occurred loading the mod configuration file.", MOD_NAME, VERSION);
+	}
+}
 
 bool IsNumeric(RValue value)
 {
@@ -187,28 +322,31 @@ RValue& GmlScriptDrawMonsterCallback(
 	int sprite_height = MONSTER_NAME_TO_SPRITE_SIZE_MAP.at(monster_id_to_name_map[monster_id.ToInt64()]);
 	int bar_x = x.ToInt64();
 	int bar_y = y.ToInt64() - sprite_height;
+	int health_bar_width = configuration.smaller_health_bar ? SMALL_HEALTH_BAR_WIDTH : HEALTH_BAR_WIDTH;
+	int health_bar_height = configuration.smaller_health_bar ? SMALL_HEALTH_BAR_HEIGHT : HEALTH_BAR_HEIGHT;
 
 	double hp_percent = health.ToDouble() / max_health.ToDouble();
 	hp_percent = std::clamp(hp_percent, 0.0, 1.0);
 
 	// Centered bounds
-	int left = bar_x - HEALTH_BAR_WIDTH / 2;
-	int right = bar_x + HEALTH_BAR_WIDTH / 2;
-	int top = bar_y - HEALTH_BAR_HEIGHT / 2;
-	int bottom = bar_y + HEALTH_BAR_HEIGHT / 2;
+	int left = bar_x - health_bar_width / 2;
+	int right = bar_x + health_bar_width / 2;
+	int top = bar_y - health_bar_height / 2;
+	int bottom = bar_y + health_bar_height / 2;
 
-	// Fill only middle 3 rows
-	int fill_top = top + 3;
-	int fill_bottom = top + 6;
+	// Fill only middle rows
+	int fill_top = top + health_bar_height / 3;
+	int fill_bottom = top + (health_bar_height * 2) / 3;
 
 	// Compute fill width
-	int fill_right = left + (int)(HEALTH_BAR_WIDTH * hp_percent) - 1;
+	int fill_right = left + (int)(health_bar_width * hp_percent) - 1;
 
 	// Draw fill (black background first)
 	DrawRectangle(0, left, fill_top, right - 1, fill_bottom, false);
 
-	// Draw green HP
-	DrawRectangle(65280, left, fill_top, fill_right, fill_bottom, false);
+	// Draw HP
+	int health_bar_color = configuration.red_health_bar ? 255 : 65280;
+	DrawRectangle(health_bar_color, left, fill_top, fill_right, fill_bottom, false);
 
 	RValue sprite_index = g_ModuleInterface->CallBuiltin(
 		"asset_get_index", {
@@ -218,12 +356,17 @@ RValue& GmlScriptDrawMonsterCallback(
 
 	// Draw sprite on top (centered)
 	g_ModuleInterface->CallBuiltin(
-		"draw_sprite",
+		"draw_sprite_ext",
 		{
 			RValue(sprite_index),
 			RValue(-1),
 			RValue(left),
-			RValue(top)
+			RValue(top),
+			RValue((double)health_bar_width / HEALTH_BAR_WIDTH),
+			RValue((double)health_bar_height / HEALTH_BAR_HEIGHT),
+			RValue(0),
+			RValue(16777215),
+			RValue(1.0)
 		}
 	);
 
@@ -243,7 +386,7 @@ RValue& GmlScriptSetupMainScreenCallback(
 		load_on_start = false;
 		g_ModuleInterface->GetGlobalInstance(&global_instance);
 		LoadMonsters();
-		//CreateOrLoadConfigFile();
+		CreateOrLoadConfigFile();
 	}
 
 	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, GML_SCRIPT_SETUP_MAIN_SCREEN));
