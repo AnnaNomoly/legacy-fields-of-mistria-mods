0c256968fb6cf524eab16cb36a5a99fe8ed441ca
AnnaNomoly <annanomoly@outlook.com>
2026-05-15 14:56:00 -0400

    MMAPI updates

--- InfiniteHealth/source/ModuleMain.cpp
+++ InfiniteHealth/source/ModuleMain.cpp
@@ -1,69 +1,23 @@
-#include <YYToolkit/YYTK_Shared.hpp> // YYTK v4
+#include <YYToolkit/YYTK_Shared.hpp>
+#include <MMAPI/MMAPI.hpp>
 using namespace Aurie;
 using namespace YYTK;
 
 static const char* const MOD_NAME = "InfiniteHealth";
-static const char* const VERSION = "1.0.2";
-static const char* const GML_SCRIPT_MODIFY_HEALTH = "gml_Script_modify_health@Ari@Ari";
+static const char* const VERSION = "1.1.0";
 
 static YYTKInterface* g_ModuleInterface = nullptr;
 
-RValue& GmlScriptModifyHealthCallback(
-	IN CInstance* Self,
-	IN CInstance* Other,
-	OUT RValue& Result,
-	IN int ArgumentCount,
-	IN RValue** Arguments
-)
+static void BeforeHealthChange(MMAPI::Player::BeforeHealthChangeContext& ctx)
 {
-	if (Arguments[0]->m_Real < 0)
-		Arguments[0]->m_Real = 0;
-
-	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, GML_SCRIPT_MODIFY_HEALTH));
-	original(
-		Self,
-		Other,
-		Result,
-		ArgumentCount,
-		Arguments
-	);
-
-	return Result;
-}
-
-void CreateHookGmlScriptModifyHealth(AurieStatus& status)
-{
-	CScript* gml_script_modify_health = nullptr;
-	status = g_ModuleInterface->GetNamedRoutinePointer(
-		GML_SCRIPT_MODIFY_HEALTH,
-		(PVOID*)&gml_script_modify_health
-	);
-
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to get script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_MODIFY_HEALTH);
-	}
-
-	status = MmCreateHook(
-		g_ArSelfModule,
-		GML_SCRIPT_MODIFY_HEALTH,
-		gml_script_modify_health->m_Functions->m_ScriptFunction,
-		GmlScriptModifyHealthCallback,
-		nullptr
-	);
-
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to hook script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_MODIFY_HEALTH);
-	}
+	if (ctx.GetAmount() < 0)
+		ctx.SetAmount(0);
 }
 
 EXPORTED AurieStatus ModuleInitialize(IN AurieModule* Module, IN const fs::path& ModulePath) {
 	UNREFERENCED_PARAMETER(ModulePath);
 
-	AurieStatus status = AURIE_SUCCESS;
-
-	status = ObGetInterface(
+	AurieStatus status = ObGetInterface(
 		"YYTK_Main",
 		(AurieInterfaceBase*&)(g_ModuleInterface)
 	);
@@ -73,13 +27,13 @@ EXPORTED AurieStatus ModuleInitialize(IN AurieModule* Module, IN const fs::path&
 
 	g_ModuleInterface->Print(CM_LIGHTAQUA, "[%s %s] - Plugin starting...", MOD_NAME, VERSION);
 
-	CreateHookGmlScriptModifyHealth(status);
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Exiting due to failure on start!", MOD_NAME, VERSION);
-		return status;
-	}
+	CInstance* global_instance = nullptr;
+	g_ModuleInterface->GetGlobalInstance(&global_instance);
+	MMAPI::Initialize(g_ModuleInterface, global_instance, g_ArSelfModule, MOD_NAME, VERSION);
 
-	g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Plugin started!", MOD_NAME, VERSION);
+	MMAPI::Player::Enable();
+	MMAPI::Player::Hooks::BeforeHealthChange(BeforeHealthChange);
+
+	MMAPI::Log::Info("Plugin started!");
 	return AURIE_SUCCESS;
 }
